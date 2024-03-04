a1d4e2bfdc3a7f4a5761f4dc7e9029660c1256aa
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-03-04 17:03:15 +0900

    start

---
+++ src/main/java/com/minsta/m/MApplication.java
@@ -0,0 +1,13 @@
+package com.minsta.m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/minsta/m/MApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minsta.m;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
