bcd5c15bc6fd17472b6404b42bae946037608790
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-03-07 12:15:30 +0900

    add :: user controller handling

--- src/main/java/com/minsta/m/domain/user/controller/UserController.java
+++ src/main/java/com/minsta/m/domain/user/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.minsta.m.domain.user.controller;
 
+import com.minsta.m.domain.user.controller.data.request.LoginRequest;
 import com.minsta.m.domain.user.controller.data.request.SignupRequest;
 import com.minsta.m.domain.user.controller.data.request.SmsAuthRequest;
 import com.minsta.m.domain.user.controller.data.request.SmsSendRequest;
+import com.minsta.m.domain.user.controller.data.response.LoginResponse;
+import com.minsta.m.domain.user.service.LoginService;
 import com.minsta.m.domain.user.service.SignupService;
 import com.minsta.m.domain.user.service.SmsAuthenticationService;
 import com.minsta.m.domain.user.service.SmsSendService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -21,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final SignupService signupService;
+    private final LoginService loginService;
     private final SmsSendService smsSendService;
     private final SmsAuthenticationService smsAuthenticationService;
 
@@ -28,6 +33,12 @@ public class UserController {
     public ResponseEntity<HttpStatus> signup(@RequestBody @Valid SignupRequest signupRequest) {
         signupService.execute(signupRequest);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PostMapping()
+    public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest loginRequest, HttpServletResponse response) {
+        var res = loginService.execute(loginRequest, response);
+        return new ResponseEntity<>(res, HttpStatus.OK);
     }
 
     @PostMapping("/auth")
