e979234211efb852f0b7ed6880b2b5528817e1b3
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-03-08 15:19:58 +0900

    add :: file service

---
+++ src/main/java/com/minsta/m/domain/file/service/impl/FileUploadServiceImpl.java
@@ -0,0 +1,90 @@
+package com.minsta.m.domain.file.service.impl;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.minsta.m.domain.file.controller.data.response.FileUploadResponse;
+import com.minsta.m.domain.file.exception.FileUploadFailedException;
+import com.minsta.m.domain.file.exception.InvalidFormatFileException;
+import com.minsta.m.domain.file.exception.NotAllowedFileException;
+import com.minsta.m.domain.file.service.FileUploadService;
+import com.minsta.m.global.annotation.ServiceWithTransactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@ServiceWithTransactional
+public class FileUploadServiceImpl implements FileUploadService {
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    private final AmazonS3 amazonS3;
+
+    private static final List<String> ALLOWED_MIME_TYPES = Arrays.asList("image/jpg", "image/png", "image/gif");
+
+    @Override
+    public FileUploadResponse execute(MultipartFile file) {
+        String fileName = createFileName(file.getOriginalFilename());
+
+        String contentType = file.getContentType();
+
+        if(!ALLOWED_MIME_TYPES.contains(contentType)) {
+            throw new NotAllowedFileException();
+        }
+
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+
+        objectMetadata.setContentLength(file.getSize());
+
+        objectMetadata.setContentType(contentType);
+
+        try (InputStream inputStream = file.getInputStream()) {
+
+            amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+        } catch (IOException e) {
+            throw new FileUploadFailedException();
+        }
+
+        String fileUrl = generateFileUrl(fileName);
+
+        return FileUploadResponse.builder()
+                .awsUrl(fileUrl)
+                .build();
+    }
+
+    public void deleteFile(String fileName) {
+
+        amazonS3.deleteObject(new DeleteObjectRequest(bucket, fileName));
+    }
+
+    private String createFileName(String fileName) {
+
+        return UUID.randomUUID().toString().concat(getFileExtension(fileName));
+    }
+
+    private String getFileExtension(String fileName) {
+
+        try {
+
+            return fileName.substring(fileName.lastIndexOf("."));
+        } catch (StringIndexOutOfBoundsException e) {
+
+            throw new InvalidFormatFileException();
+        }
+    }
+
+    private String generateFileUrl(String fileName) {
+        return amazonS3.getUrl(bucket, fileName).toString();
+    }
+}
