66d770d0aaac311219ca1f7713cbc5a972b9e71e
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-03-08 15:16:25 +0900

    fix :: @id import error

--- src/main/java/com/minsta/m/domain/auth/entity/BlackList.java
+++ src/main/java/com/minsta/m/domain/auth/entity/BlackList.java
@@ -1,10 +1,10 @@
 package com.minsta.m.domain.auth.entity;
 
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 import org.springframework.data.redis.core.index.Indexed;
