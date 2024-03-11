21e43df6cfb48418a75d0c8ec7578a491ad7a244
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-03-11 13:49:36 +0900

    add :: create service

---
+++ src/main/java/com/minsta/m/domain/leels/service/impl/CreateLeelsCommentServiceImpl.java
@@ -0,0 +1,31 @@
+package com.minsta.m.domain.leels.service.impl;
+
+import com.minsta.m.domain.leels.entity.LeelsComment;
+import com.minsta.m.domain.leels.controller.data.request.CreateLeelsCommentRequest;
+import com.minsta.m.domain.leels.repository.LeelsCommentRepository;
+import com.minsta.m.domain.leels.service.CreateLeelsCommentService;
+import com.minsta.m.global.annotation.ServiceWithTransactional;
+import com.minsta.m.global.util.LeelsUtil;
+import com.minsta.m.global.util.UserUtil;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@ServiceWithTransactional
+public class CreateLeelsCommentServiceImpl implements CreateLeelsCommentService {
+
+    private final LeelsCommentRepository leelsCommentRepository;
+    private final UserUtil userUtil;
+    private final LeelsUtil leelsUtil;
+
+    @Override
+    public void execute(CreateLeelsCommentRequest createLeelsCommentRequest, Long leelsId) {
+
+        LeelsComment leelsComment = LeelsComment.builder()
+                .leels(leelsUtil.getLeels(leelsId))
+                .user(userUtil.getUser())
+                .comment(createLeelsCommentRequest.getComment())
+                .build();
+
+        leelsCommentRepository.save(leelsComment);
+    }
+}
