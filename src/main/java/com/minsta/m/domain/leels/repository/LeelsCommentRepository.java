374edb469159d23a8aba1c333c9238cf574fe3e9
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-03-12 20:32:18 +0900

    add :: jpa method

--- src/main/java/com/minsta/m/domain/leels/repository/LeelsCommentRepository.java
+++ src/main/java/com/minsta/m/domain/leels/repository/LeelsCommentRepository.java
@@ -1,7 +1,10 @@
 package com.minsta.m.domain.leels.repository;
 
+import com.minsta.m.domain.leels.entity.Leels;
 import com.minsta.m.domain.leels.entity.LeelsComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LeelsCommentRepository extends JpaRepository<LeelsComment, Long> {
+
+    LeelsComment findByLeelsCommentIdAndLeels(Long leelsCommentId, Leels leels);
 }
