61f38b3129c9f43c5a43ec73167c05f9f6b526af
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-03-14 08:42:09 +0900

    add :: error code

--- src/main/java/com/minsta/m/global/error/ErrorCode.java
+++ src/main/java/com/minsta/m/global/error/ErrorCode.java
@@ -35,6 +35,7 @@ public enum ErrorCode {
     DELETE_PERMISSION_DENIED("작성자만 삭제가 가능", 403),
     UPDATE_PERMISSION_DENIED("작성자만 수정이 가능", 403),
     REPLY_COMMENT_NOT_FOUND("답글이 존재하지않습니다.", 404),
+    CANCEL_PERMISSION_DENIED("좋아요 취소는 본인만 가능합니다", 403),
 
     //Client
     BAD_REQUEST("잘못된 요청",400);
