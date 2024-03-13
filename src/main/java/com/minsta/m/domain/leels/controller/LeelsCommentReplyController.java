596c5c24581409ee3cdedbe8630b59c4f9ad4d0a
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-03-13 15:48:43 +0900

    add :: controller mapping

---
+++ src/main/java/com/minsta/m/domain/leels/controller/LeelsCommentReplyController.java
@@ -0,0 +1,50 @@
+package com.minsta.m.domain.leels.controller;
+
+import com.minsta.m.domain.leels.controller.data.request.CreateLeelsCommentRequest;
+import com.minsta.m.domain.leels.service.CreateLeelsCommentReplyService;
+import com.minsta.m.domain.leels.service.DeleteLeelsCommentReplyService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/leels/{leelsId}/{leelsCommentId}")
+@RequiredArgsConstructor
+public class LeelsCommentReplyController {
+
+    private final CreateLeelsCommentReplyService createLeelsCommentReplyService;
+    private final DeleteLeelsCommentReplyService deleteLeelsCommentReplyService;
+
+    @PostMapping("/reply")
+    public ResponseEntity<HttpStatus> createCommentReply(
+            @RequestBody @Valid CreateLeelsCommentRequest replyRequest,
+            @PathVariable Long leelsId,
+            @PathVariable Long leelsCommentId
+    ) {
+        createLeelsCommentReplyService.execute(leelsId, leelsCommentId, replyRequest);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{leelsCommentReplyId}")
+    public ResponseEntity<HttpStatus> deleteCommentReply(
+            @PathVariable Long leelsId,
+            @PathVariable Long leelsCommentId,
+            @PathVariable Long leelsCommentReplyId
+    ) {
+        deleteLeelsCommentReplyService.execute(leelsId, leelsCommentId, leelsCommentReplyId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PatchMapping("/{leelCommentReplyId}")
+    public ResponseEntity<HttpStatus> updateCommentReply(
+            @PathVariable Long leelsId,
+            @PathVariable Long leelsCommentId,
+            @PathVariable Long leelsCommentReplyId,
+            @RequestBody @Valid CreateLeelsCommentRequest updateRequest
+    ) {
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
