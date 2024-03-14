a4dbe5db4d728b66b18ae1bd17551db73a9f85cb
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-03-14 13:55:34 +0900

    add :: controller mapping

--- src/main/java/com/minsta/m/domain/leels/controller/LeelsController.java
+++ src/main/java/com/minsta/m/domain/leels/controller/LeelsController.java
@@ -1,6 +1,8 @@
 package com.minsta.m.domain.leels.controller;
 
 import com.minsta.m.domain.leels.controller.data.request.CreateLeelsRequest;
+import com.minsta.m.domain.leels.service.CancelLeelsLikeService;
+import com.minsta.m.domain.leels.service.CreateLeelsLikeService;
 import com.minsta.m.domain.leels.service.CreateLeelsService;
 import com.minsta.m.domain.leels.service.LeelsDeleteService;
 import jakarta.validation.Valid;
@@ -16,6 +18,8 @@ public class LeelsController {
 
     private final CreateLeelsService createLeelsService;
     private final LeelsDeleteService leelsDeleteService;
+    private final CreateLeelsLikeService createLeelsLikeService;
+    private final CancelLeelsLikeService cancelLeelsLikeService;
 
     @PostMapping
     public ResponseEntity<HttpStatus> create(@RequestBody @Valid CreateLeelsRequest createLeelsRequest) {
@@ -26,6 +30,18 @@ public class LeelsController {
     @DeleteMapping("/{leelsId}")
     public ResponseEntity<HttpStatus> delete(@PathVariable Long leelsId) {
         leelsDeleteService.execute(leelsId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping("/{leelsId}")
+    public ResponseEntity<HttpStatus> like(@PathVariable Long leelsId) {
+        createLeelsLikeService.execute(leelsId);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/{leelsId}")
+    public ResponseEntity<HttpStatus> cancel(@PathVariable Long leelsId) {
+        createLeelsLikeService.execute(leelsId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
