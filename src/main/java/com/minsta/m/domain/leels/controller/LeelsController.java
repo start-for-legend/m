104b956dd688ac67892dbf0ad5b3be5947c9f16d
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-03-14 11:18:14 +0900

    add :: controller mapping

--- src/main/java/com/minsta/m/domain/leels/controller/LeelsController.java
+++ src/main/java/com/minsta/m/domain/leels/controller/LeelsController.java
@@ -2,14 +2,12 @@ package com.minsta.m.domain.leels.controller;
 
 import com.minsta.m.domain.leels.controller.data.request.CreateLeelsRequest;
 import com.minsta.m.domain.leels.service.CreateLeelsService;
+import com.minsta.m.domain.leels.service.LeelsDeleteService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/leels")
@@ -17,10 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class LeelsController {
 
     private final CreateLeelsService createLeelsService;
+    private final LeelsDeleteService leelsDeleteService;
 
     @PostMapping
     public ResponseEntity<HttpStatus> create(@RequestBody @Valid CreateLeelsRequest createLeelsRequest) {
         createLeelsService.execute(createLeelsRequest);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{leelsId}")
+    public ResponseEntity<HttpStatus> delete(@PathVariable Long leelsId) {
+        leelsDeleteService.execute(leelsId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
