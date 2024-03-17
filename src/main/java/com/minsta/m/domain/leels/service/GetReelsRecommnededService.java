b1c4ad737d5b70ccb92c1f8f73cb3389dc838428
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-03-17 15:52:23 +0900

    add :: get reels recommended service

---
+++ src/main/java/com/minsta/m/domain/leels/service/GetReelsRecommnededService.java
@@ -0,0 +1,10 @@
+package com.minsta.m.domain.leels.service;
+
+import com.minsta.m.domain.leels.controller.data.response.LeelsResponse;
+
+import java.util.List;
+
+public interface GetReelsRecommnededService {
+
+    List<LeelsResponse> execute();
+}
