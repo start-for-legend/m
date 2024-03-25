bf933764bdf2bf13c20bc9a48acc4938d6b4af5a
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-03-25 15:39:38 +0900

    add :: controller swagger

--- src/main/java/com/minsta/m/domain/auth/controller/AuthController.java
+++ src/main/java/com/minsta/m/domain/auth/controller/AuthController.java
@@ -3,6 +3,13 @@ package com.minsta.m.domain.auth.controller;
 import com.minsta.m.domain.auth.service.LogoutService;
 import com.minsta.m.domain.auth.service.ReIssueTokenService;
 import com.minsta.m.domain.user.controller.data.response.LoginResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +20,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "http://10.53.68.120:80/auth 하위 API", description = "Auth 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
@@ -21,12 +29,36 @@ public class AuthController {
     private final ReIssueTokenService reIssueTokenService;
     private final LogoutService logoutService;
 
+
+    @Operation(summary = "reissue token", description = "유저 토큰 재발급")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "OK",
+                    content = @Content(schema = @Schema(implementation = LoginResponse.class)),
+                    headers = @Header(name = "refreshToken", description = "refreshToken value", required = true)
+            ),
+            @ApiResponse(responseCode = "401", description = "Token InValid, Token Expired"),
+            @ApiResponse(responseCode = "500", description = "Server Error"),
+    })
+
     @PatchMapping
     public ResponseEntity<LoginResponse> reIssueToken(HttpServletRequest request, HttpServletResponse response) {
         var res = reIssueTokenService.execute(request.getCookies(), response);
         return new ResponseEntity<>(res, HttpStatus.OK);
     }
 
+
+    @Operation(summary = "logout", description = "유저 로그아웃")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "NO_CONTENT",
+                    headers = @Header(name = "refreshToken", description = "refreshToken value", required = true)
+            ),
+            @ApiResponse(responseCode = "401", description = "Token InValid, Token Expired"),
+            @ApiResponse(responseCode = "500", description = "Server Error"),
+    })
     @DeleteMapping
     public ResponseEntity<HttpStatus> logout(HttpServletRequest request) {
         logoutService.execute(request);
--- src/main/java/com/minsta/m/domain/leels/controller/LeelsCommentReplyController.java
+++ src/main/java/com/minsta/m/domain/leels/controller/LeelsCommentReplyController.java
@@ -2,12 +2,19 @@ package com.minsta.m.domain.leels.controller;
 
 import com.minsta.m.domain.leels.controller.data.request.CreateLeelsCommentRequest;
 import com.minsta.m.domain.leels.service.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
+@Tag(name = "http://10.53.68.120:80/leels/{leelsId}/{leelsCommentId} 하위 API", description = "leels comment reply 관련 API")
 @RestController
 @RequestMapping("/leels/{leelsId}/{leelsCommentId}")
 @RequiredArgsConstructor
@@ -19,6 +26,15 @@ public class LeelsCommentReplyController {
     private final LeelsCommentReplyLikeService leelsCommentReplyLikeService;
     private final LeelsCommentReplyLikeCancelService leelsCommentReplyLikeCancelService;
 
+    @Operation(summary = "Create Comment Reply", description = "댓글에 대한 답글 생성")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Created Reply",
+                    headers = @Header(name = "accessToken",description = "accessToken Value", required = true)),
+            @ApiResponse(responseCode = "400", description = "Bad Request"),
+            @ApiResponse(responseCode = "401", description = "Invalid Token, Token Expired"),
+            @ApiResponse(responseCode = "404", description = "leels or leelscomment not found"),
+            @ApiResponse(responseCode = "500", description = "Server Error")
+    })
     @PostMapping("/reply")
     public ResponseEntity<HttpStatus> createCommentReply(
             @RequestBody @Valid CreateLeelsCommentRequest replyRequest,
@@ -29,6 +45,16 @@ public class LeelsCommentReplyController {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Delete Comment Reply", description = "댓글에 대한 답글 삭제")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Delete Reply",
+                    headers = @Header(name = "accessToken",description = "accessToken Value", required = true)),
+            @ApiResponse(responseCode = "400", description = "Bad Request"),
+            @ApiResponse(responseCode = "401", description = "Invalid Token, Token Expired"),
+            @ApiResponse(responseCode = "403", description = "Not Your Reply"),
+            @ApiResponse(responseCode = "404", description = "leels or leelscomment not found"),
+            @ApiResponse(responseCode = "500", description = "Server Error")
+    })
     @DeleteMapping("/{leelsCommentReplyId}")
     public ResponseEntity<HttpStatus> deleteCommentReply(
             @PathVariable Long leelsId,
@@ -39,6 +65,16 @@ public class LeelsCommentReplyController {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+    @Operation(summary = "update Comment Reply", description = "댓글에 대한 답글 수정")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Update Reply",
+                    headers = @Header(name = "accessToken",description = "accessToken Value", required = true)),
+            @ApiResponse(responseCode = "400", description = "Bad Request"),
+            @ApiResponse(responseCode = "401", description = "Invalid Token, Token Expired"),
+            @ApiResponse(responseCode = "403", description = "Not Your Reply"),
+            @ApiResponse(responseCode = "404", description = "leels or leelscomment not found"),
+            @ApiResponse(responseCode = "500", description = "Server Error")
+    })
     @PatchMapping("/{leelsCommentReplyId}")
     public ResponseEntity<HttpStatus> updateCommentReply(
             @PathVariable Long leelsId,
@@ -50,6 +86,15 @@ public class LeelsCommentReplyController {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+    @Operation(summary = "Like Comment Reply", description = "댓글에 대한 답글 좋아요 누르기")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Create Like Reply",
+                    headers = @Header(name = "accessToken",description = "accessToken Value", required = true)),
+            @ApiResponse(responseCode = "400", description = "Bad Request"),
+            @ApiResponse(responseCode = "401", description = "Invalid Token, Token Expired"),
+            @ApiResponse(responseCode = "404", description = "leels or leelscomment not found"),
+            @ApiResponse(responseCode = "500", description = "Server Error")
+    })
     @PostMapping("/{leelsCommentReplyId}")
     public ResponseEntity<HttpStatus> likeCommentReply(
             @PathVariable Long leelsId,
@@ -60,6 +105,16 @@ public class LeelsCommentReplyController {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Delete Comment Reply Like", description = "댓글에 대한 답글 좋아요 취소")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Delete Reply Like",
+                    headers = @Header(name = "accessToken",description = "accessToken Value", required = true)),
+            @ApiResponse(responseCode = "400", description = "Bad Request"),
+            @ApiResponse(responseCode = "401", description = "Invalid Token, Token Expired"),
+            @ApiResponse(responseCode = "403", description = "Not Your Like"),
+            @ApiResponse(responseCode = "404", description = "leels or leelscomment not found"),
+            @ApiResponse(responseCode = "500", description = "Server Error")
+    })
     @DeleteMapping("/{leelsCommentReplyId}/like")
     public ResponseEntity<HttpStatus> likeCommentReplyCancel(
             @PathVariable Long leelsId,
