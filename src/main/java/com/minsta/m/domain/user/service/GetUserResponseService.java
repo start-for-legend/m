1621a9248236fb0b04bd53f2bb3e7a816581c187
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-03-26 18:25:01 +0900

    add :: get user response

---
+++ src/main/java/com/minsta/m/domain/user/service/GetUserResponseService.java
@@ -0,0 +1,8 @@
+package com.minsta.m.domain.user.service;
+
+import com.minsta.m.domain.user.controller.data.response.UserResponse;
+
+public interface GetUserResponseService {
+
+    UserResponse execute();
+}
