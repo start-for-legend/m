ff255d394c669e1f9cef4a4f138b31317f0dc18e
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-03-26 10:45:58 +0900

    add :: create notice 적용

---
+++ src/main/java/com/minsta/m/global/util/CreateNotice.java
@@ -0,0 +1,31 @@
+package com.minsta.m.global.util;
+
+import com.minsta.m.domain.notice.entity.Notice;
+import com.minsta.m.domain.notice.repository.NoticeRepository;
+import com.minsta.m.global.annotation.ServiceWithTransactional;
+import com.minsta.m.global.util.request.NoticeRequest;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@ServiceWithTransactional
+public class CreateNotice {
+
+    private static final String SERVER = "http://10.53.68.120:80/";
+    private final NoticeRepository noticeRepository;
+
+    public void createNotice(NoticeRequest data) {
+        Notice notice = Notice.builder()
+                .noticeType(data.noticeType())
+                .user(data.user())
+                .isRead(false)
+                .isValid(false)
+                .url(SERVER + data.url())
+                .createAt(LocalDateTime.now())
+                .receiverId(data.receiverId())
+                .build();
+
+        noticeRepository.save(notice);
+    }
+}
