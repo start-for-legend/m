d4b94b8940e4097df46a569bd0ae9ee0c5a641aa
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-03-27 13:41:11 +0900

    update :: follow service

--- src/main/java/com/minsta/m/domain/follow/service/impl/FollowServiceImpl.java
+++ src/main/java/com/minsta/m/domain/follow/service/impl/FollowServiceImpl.java
@@ -7,6 +7,7 @@ import com.minsta.m.domain.follow.service.FollowService;
 import com.minsta.m.domain.user.entity.User;
 import com.minsta.m.domain.user.repository.UserRepository;
 import com.minsta.m.global.annotation.ServiceWithTransactional;
+import com.minsta.m.global.redis.RedisList;
 import com.minsta.m.global.security.exception.UserNotFoundException;
 import com.minsta.m.global.util.UserUtil;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,7 @@ public class FollowServiceImpl implements FollowService {
     private final UserUtil userUtil;
     private final UserRepository userRepository;
     private final FollowRepository followRepository;
+    private final RedisList redisList;
 
     @Override
     public void execute(Long userId) {
@@ -34,5 +36,10 @@ public class FollowServiceImpl implements FollowService {
                 .build();
 
         followRepository.save(follow);
+        storeRedis(followed);
+    }
+
+    private void storeRedis(User user) {
+        redisList.addFollowing(String.valueOf(user.getUserId()), user.getNickName());
     }
 }
