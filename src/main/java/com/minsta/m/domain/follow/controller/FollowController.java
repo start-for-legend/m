c35b401d55a74f91d5e797aaf9befcd68c0ad01c
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-03-27 09:46:18 +0900

    fix :: column name

--- src/main/java/com/minsta/m/domain/follow/controller/FollowController.java
+++ src/main/java/com/minsta/m/domain/follow/controller/FollowController.java
@@ -30,8 +30,8 @@ public class FollowController {
             @ApiResponse(responseCode = "500", description = "Internal Server Error, 서버 에러")
     })
     @PostMapping("/{uesrId}")
-    public ResponseEntity<HttpStatus> follow(@PathVariable Long uesrId) {
-        followService.execute(uesrId);
+    public ResponseEntity<HttpStatus> follow(@PathVariable Long userId) {
+        followService.execute(userId);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
