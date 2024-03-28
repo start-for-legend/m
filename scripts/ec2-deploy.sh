d7d402965cbd4ecc243bc516b7f5caff7eb75a7e
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-03-28 11:22:55 +0900

    add :: deploy script

---
+++ scripts/ec2-deploy.sh
@@ -0,0 +1,13 @@
+CONFIG_PATH="/home/ubuntu/docker-compose.yml"
+
+sudo docker-compose pull
+
+if [ ! -e /home/ubuntu/.initialized ];then
+  echo "First time running"
+  sudo docker-compose -f $CONFIG_PATH up redis mysql -d --wait
+  touch /home/ubuntu/.initialized
+fi
+
+sudo docker-compose -f $CONFIG_PATH up app -d
+
+sudo docker image prune -af
