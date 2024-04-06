52f6badbe89c6dfa2144a66bf3820c3c0c3a8d98
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-04-07 01:30:05 +0900

    set :: feed comment oneToMany relation

--- src/main/java/com/minsta/m/domain/feed/entity/feedcomment/FeedComment.java
+++ src/main/java/com/minsta/m/domain/feed/entity/feedcomment/FeedComment.java
@@ -1,10 +1,14 @@
 package com.minsta.m.domain.feed.entity.feedcomment;
 
 import com.minsta.m.domain.feed.entity.feed.Feed;
+import com.minsta.m.domain.feed.entity.feedcommentreply.FeedCommentReply;
 import com.minsta.m.domain.user.entity.User;
 import com.minsta.m.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -26,8 +30,12 @@ public class FeedComment extends BaseEntity {
     private User user;
 
     @ManyToOne
-    @JoinColumn(name = "feed")
+    @JoinColumn(name = "feed_id")
     private Feed feed;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "feedComment", cascade = CascadeType.ALL)
+    private List<FeedCommentReply> comments = new ArrayList<>();
 
     public void setContent(String content) {
         this.content = content;
