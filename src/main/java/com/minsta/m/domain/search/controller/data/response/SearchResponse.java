319e8294376f9051d66be7c7d4c27bef02540571
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-04-11 09:54:28 +0900

    map :: controller

---
+++ src/main/java/com/minsta/m/domain/search/controller/data/response/SearchResponse.java
@@ -0,0 +1,19 @@
+package com.minsta.m.domain.search.controller.data.response;
+
+import com.minsta.m.domain.user.controller.data.response.UserResponse;
+
+import java.util.Map;
+
+public record SearchResponse(
+        Map<String, Long> hashTagResponses,
+        Map<UserResponse, Long> userResponses
+) {
+
+    public static SearchResponse forHashTag(Map<String, Long> hashtags) {
+        return new SearchResponse(hashtags, null);
+    }
+
+    public static SearchResponse forKeyWard(Map<String, Long> hashtags, Map<UserResponse, Long> userResponses) {
+        return new SearchResponse(hashtags, userResponses);
+    }
+}
