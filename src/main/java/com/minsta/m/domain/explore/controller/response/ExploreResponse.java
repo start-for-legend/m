b3740de9c22fee41778023dcb887b129b0f435e5
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-04-11 14:43:55 +0900

    add :: explore service

---
+++ src/main/java/com/minsta/m/domain/explore/controller/response/ExploreResponse.java
@@ -0,0 +1,16 @@
+package com.minsta.m.domain.explore.controller.response;
+
+import com.minsta.m.domain.feed.controller.data.response.FeedResponse;
+import com.minsta.m.domain.leels.controller.data.response.LeelsResponse;
+
+import java.util.List;
+
+public record ExploreResponse(
+        List<FeedResponse> feedResponse,
+        List<LeelsResponse> leelsResponse
+) {
+
+    public static ExploreResponse of(List<FeedResponse> feedResponse, List<LeelsResponse> leelsResponse) {
+        return new ExploreResponse(feedResponse, leelsResponse);
+    }
+}
---
+++ src/main/java/com/minsta/m/domain/explore/service/impl/ExploreServiceImpl.java
@@ -0,0 +1,58 @@
+package com.minsta.m.domain.explore.service.impl;
+
+import com.minsta.m.domain.explore.controller.response.ExploreResponse;
+import com.minsta.m.domain.explore.service.ExploreService;
+import com.minsta.m.domain.feed.controller.data.response.FeedResponse;
+import com.minsta.m.domain.leels.service.GetReelsRecommendedService;
+import com.minsta.m.domain.user.controller.data.response.UserResponse;
+import com.minsta.m.global.annotation.ReadOnlyService;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.minsta.m.domain.feed.entity.feed.QFeed.feed;
+import static com.minsta.m.domain.user.entity.QUser.user;
+import static com.minsta.m.domain.feed.entity.feed.QFeedLike.feedLike;
+
+@ReadOnlyService
+@RequiredArgsConstructor
+public class ExploreServiceImpl implements ExploreService {
+
+    private final JPAQueryFactory em;
+    private final GetReelsRecommendedService getReelsRecommendedService;
+
+    @Override
+    public ExploreResponse execute(int page) {
+
+        var res = em
+                .select(Projections.constructor(
+                        FeedResponse.class,
+                        feed.feedId,
+                        Projections.constructor(UserResponse.class, // 중첩 Projections
+                                user.userId,
+                                user.nickName,
+                                user.profileUrl,
+                                user.name),
+                        feed.content,
+                        Expressions.list(feed.hashtags),
+                        Expressions.list(feed.fileUrls)
+                ))
+                .from(feed)
+                .leftJoin(feedLike).on(feedLike.feed.feedId.eq(feed.feedId))
+                .leftJoin(user).on(feed.user.userId.eq(user.userId))
+                .groupBy(feed.feedId)
+                .orderBy(feedLike.count().desc())
+                .limit(15)
+                .offset((page - 1) * 15L)
+                .fetch();
+
+        var response = getReelsRecommendedService.execute();
+
+        return ExploreResponse.of(res, new ArrayList<>(response.subList(5, response.size())));
+    }
+}
