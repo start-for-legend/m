d4cf2fd345f730042e52ef7c83ce533e9ce3f4a2
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-04-24 20:22:52 +0900

    fix ::

--- src/main/java/com/minsta/m/global/error/ErrorCode.java
+++ src/main/java/com/minsta/m/global/error/ErrorCode.java
@@ -24,7 +24,6 @@ public enum ErrorCode {
 
     //AWS
     FILE_UPLOAD_FAIL("파일 업로드에 실패했습니다.", 500),
-    NOT_ALLOWED_FILE("허용되지 않은 파일 형식입니다.", 400),
     INVALID_FORMAT_FILE("잘못된 형식의 파일입니다.", 400),
 
     //Leels
